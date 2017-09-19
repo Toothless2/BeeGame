749720473801d8510be06e504c630ccb95d517d8
Toothless <max.rose366@gmail.com>
2017-09-19 14:07:42 +0100

    vf

--- Code/BeeGame/BeeGame/MainMenu/MainMenuManager.cs
+++ Code/BeeGame/BeeGame/MainMenu/MainMenuManager.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using BeeGame.Core;
 
 namespace BeeGame.MainMenu
 {
